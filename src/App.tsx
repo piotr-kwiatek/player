102743760ed2febaf09d177042027e992fff9957
Piotr Kwiatek <piotr.kwiatek@outlook.com>
2023-06-14 19:25:40 +0200

    Add exmaple

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [isMusicDownloaded] = useState<boolean>(false);
+  const [isMusicPlaying, setIsMusicPlaying] = useState<boolean>(false);
+  const [isMusicFetched, setIsMusicFetched] = useState<boolean>(false); 
+  const audio = useRef<HTMLAudioElement>();
+
+  const handleMusic = (): void => {
+    setIsMusicPlaying(prevState => !prevState);
+  }
+  const handleDownload = (): void => {}
+
+
+  useEffect(() => {
+    if (!isMusicFetched) {
+      audio.current = new Audio(`${process.env.PUBLIC_URL}/music/combat/hip-hop-street-legal-14405.mp3`);
+      setIsMusicFetched(true);
+    }
+    isMusicPlaying ? audio.current?.play() : audio.current?.pause()
+  }, [ isMusicPlaying, isMusicFetched ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <audio src="/music/hip-hop-street-legal-14405.mp3" controls></audio>
-      </header>
+      <button type="button" onClick={handleMusic}>{isMusicPlaying ? 'Stop' : 'Play'} music</button><br/>
+      <button type="button" onClick={handleDownload}>Download music</button><br/>
+      <span>Is music downloaded: {isMusicDownloaded.toString()}.</span>
     </div>
   );
 }
